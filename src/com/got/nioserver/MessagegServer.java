5fde26665c6fe7924cc061ebc8399b70b324db6b
zzwu <zhenzhouwu@yahoo.cn>
2011-08-16 16:09:55 +0800

    NioServer init

---
+++ src/com/got/nioserver/MessagegServer.java
@@ -0,0 +1,13 @@
+package com.got.nioserver;
+
+/**
+ * 消息服务器。
+ * @author zzwu
+ *
+ */
+public class MessagegServer {
+	
+	
+	
+
+}
---
+++ src/com/got/nioserver/ReadableKeyHanlder.java
@@ -0,0 +1,5 @@
+package com.got.nioserver;
+
+public interface ReadableKeyHanlder {
+
+}
