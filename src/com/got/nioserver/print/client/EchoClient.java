487b105cd556d03aceef7508209d6132e2702c00
zzwu <zhenzhouwu@yahoo.cn>
2011-08-25 16:34:22 +0800

    all

--- src/com/got/nioserver/print/client/EchoClient.java
+++ src/com/got/nioserver/print/client/EchoClient.java
@@ -11,23 +11,14 @@ public class EchoClient {
 	public static void main(String[] args) throws Exception {
 		// Socket
 		Socket server = new Socket(InetAddress.getLocalHost(), 8888);
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				server.getInputStream()));
 		PrintWriter out = new PrintWriter(server.getOutputStream());
-		BufferedReader wt = new BufferedReader(new InputStreamReader(System.in));
-
-		while (true) {
-			String str = wt.readLine();
-			out.println(str);
-			out.flush();
-			if (str.equals("end")) {
-				break;
-			}
-			System.out.println(in.readLine());
-			break;
-		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(server.getInputStream()));
+		String sayHi = "hello!";
+		System.out.println("say hi : " + sayHi);
+		out.write(sayHi);
+		String reply = br.readLine();
+		System.out.println("get reply info:" + reply);
 		server.close();
-
 	}
 
 }
